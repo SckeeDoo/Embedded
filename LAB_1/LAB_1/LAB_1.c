3972ba5c9595d9fbd8edb5fbcd4d1483deb3507d
Schidu Vasile <sckeedoo@gmail.com>
2016-11-19 12:04:06 -0800

    First laboratory work

---
+++ LAB_1/LAB_1/LAB_1.c
@@ -0,0 +1,16 @@
+/*
+ * LAB_1.c
+ *
+ * Created: 10/2/2016 11:00:30 AM
+ *  Author: Schidu Vasile
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
