8aec4142690893896c5a9a7ab85f891ecf55aa6b
Schidu Vasile <sckeedoo@gmail.com>
2017-01-22 05:44:31 -0800

    lab added

---
+++ LAB_5/LAB_5/LAB_5.cpp
@@ -0,0 +1,16 @@
+/*
+ * LAB_5.cpp
+ *
+ * Created: 22.01.2017 04:00:24
+ *  Author: Schidu Vasile
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
